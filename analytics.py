506947abb83d57315ad4abd593db34ffc60e8c08
Tapabrata Dey <tapabratadey02@gmail.com>
2021-05-25 01:26:05 -0400

    adding scatter plots and earning predictions using linear regression

--- analytics.py
+++ analytics.py
@@ -14,6 +14,9 @@ import altair as alt
 import numpy as np
 from streamlit.elements import markdown
 import plotly.express as px
+from math import sqrt
+from random import seed, randrange
+import csv
 
 ######################################################################
 # Page Styles
@@ -196,11 +199,133 @@ with head_to_head_container:
 ######################################################################
 
 with player_earnings_header:
-	st.markdown("<h2 style='text-align: center'>Player Earning</h2>", unsafe_allow_html=True)
-	st.markdown("<h5 style='text-align: center'>Select 2 players for a head-to-head comparison</h5>", unsafe_allow_html=True)
+	st.markdown("<h2 style='text-align: center'>Player Earnings</h2>", unsafe_allow_html=True)
+	st.markdown("<h5 style='text-align: center'>Based on match winnings</h5>", unsafe_allow_html=True)
 
 ######################################################################
-# PLAYER HEAD-TO-HEAD DATA
+# PLAYER EARNINGS
 ######################################################################	
 
-# with player_earnings:
+######################################################################
+# LINEAR REGRESSION  
+# linear relationship between # of matches (x) and earnings (y)
+# y = b_0 + b_1 * x
+######################################################################	
+
+# root mean squared error
+# diff between prediction vs actual values
+def root_mean_squared_err(real, pred):
+	total_err = 0.0
+	for x in range(len(real)):
+		pred_err = pred[x] - real[x]
+		total_err += (pred_err**2)
+	err_mean = total_err / float(len(real))
+	return sqrt(err_mean)
+
+# eval algo on trained data
+def training_test_sets_split(df, split_rate):
+	training_set = list()
+	training_set_size = split_rate * len(df)
+	df_copy = list(df)
+	while len(training_set) < training_set_size:
+		idx = randrange(len(df_copy))
+		training_set.append(df_copy.pop(idx))
+	return training_set, df_copy
+
+# eval algo on trained data
+def eval_algo(df, algo, split_rate):
+	training_set, testing_set = training_test_sets_split(df, split_rate)
+	test_set = list()
+	for row in testing_set:
+		temp_row = list(row)
+		temp_row[-1] = None
+		test_set.append(temp_row)
+	pred = algo(training_set, test_set)
+	real = [row[-1] for row in testing_set]
+	sqrt_mean = root_mean_squared_err(real, pred)
+	return sqrt_mean
+
+# calculate mean
+def find_mean(vals):
+	mean = sum(vals) / float(len(vals))
+	return mean
+
+# calculate variance (how the numbers diverge)
+def find_variance(vals, mean):
+	variance = sum([(x-mean)**2 for x in vals])
+	return variance
+
+# calcualte covariance (how the numbers change together)
+def find_covariance(x_vals, x_mean, y_vals, y_mean):
+	covariance = 0.0
+	for i in range(len(x_vals)):
+		covariance += (x_vals[i] - x_mean) * (y_vals[i] - y_mean)
+	return covariance
+
+# calculate coefficients
+def find_coefficients(df):
+	x_vals = [row[0] for row in df]
+	y_vals = [row[1] for row in df]
+	x_mean = find_mean(x_vals)
+	y_mean = find_mean(y_vals)
+	b_1 = find_covariance(x_vals, x_mean, y_vals, y_mean) / find_variance(x_vals, x_mean)
+	b_0 = y_mean - b_1 * x_mean
+	fig = px.scatter(
+			x=x_vals,
+			y=y_vals,
+			title="Player Earnings from matching winnings"
+	)
+	fig.update_layout(
+			xaxis_title="Matches Won",
+			yaxis_title="Earnings (in thousands)",
+	)
+	st.plotly_chart(fig, use_container_width=True)
+	return [b_0, b_1]
+
+# predict using linear regression
+def linear_regression(trained_data, test_data):
+	pred = list()
+	x_vals = list()
+	b_0, b_1 = find_coefficients(trained_data)
+	for row in test_data:
+		# y = b_0 + b_1 * x
+		y = b_0 + b_1 * row[0]
+		pred.append(y)
+		x_vals.append(row[0])
+	fig = px.scatter(
+			x=x_vals,
+			y=pred,
+			title="Predicted player earnings using Linear Regression"
+	)
+	fig.update_layout(
+			xaxis_title="Matches Won",
+			yaxis_title="Earnings (in thousands)",
+	)
+	st.plotly_chart(fig, use_container_width=True)
+	return pred
+
+seed(1)
+def read_csv(f_name):
+	df = list()
+	with open(f_name) as earnings_csv:
+		csv_reader = csv.reader(earnings_csv)
+		for row in csv_reader:
+			if not row:
+				continue
+			if (row[0] != 'Matches Won') or (row[1] != 'Earnings'):
+				df.append(row)
+	return df
+
+# convert string to float
+def conv_to_float(df, column):
+	for row in df:
+		row[column] = float(row[column].strip())
+
+# read csv
+df = read_csv('player_earnings.csv')
+for i in range(len(df[0])):
+	conv_to_float(df, i)
+
+# 60% data is used to train the model
+rmse = eval_algo(df, linear_regression, 0.6)
+print('RMSE: %.3f' % (rmse))
